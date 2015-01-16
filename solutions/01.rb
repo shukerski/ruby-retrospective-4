1add97b43759fe7f44b5c9a46954ca6fe385e598
shukerski <shukerski@gmail.com>
2015-01-16 19:49:28 +0200

    spaces around operators fixed again

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,16 +1,16 @@
 def fibonacci(n)
-  return 1 if n==1 or n==2
-  return fibonacci(n-1)+fibonacci(n-2)
+  return 1 if n == 1 or n == 2
+  return fibonacci(n - 1)+fibonacci(n - 2)
 end
 
 def lucas(n)
-  return 2 if n==1
-  return 1 if n==2
-  return lucas(n-1)+lucas(n-2)
+  return 2 if n == 1
+  return 1 if n == 2
+  return lucas(n - 1) + lucas(n - 2)
 end
 
 def summed(n)
-  fibonacci(n)+lucas(n)
+  fibonacci(n) + lucas(n)
 end
 
 def series(name, n)
