9f2d973c9be6fcaf436fbbd48471486a7a430a6c
shukerski <shukerski@gmail.com>
2015-01-19 09:34:38 +0200

    remove unwanted return

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,12 +1,12 @@
 def fibonacci(n)
   return 1 if n == 1 or n == 2
-  return fibonacci(n - 1) + fibonacci(n - 2)
+  fibonacci(n - 1) + fibonacci(n - 2)
 end
 
 def lucas(n)
-  return 2 if n == 1
-  return 1 if n == 2
-  return lucas(n - 1) + lucas(n - 2)
+   return 2 if n == 1
+   return 1 if n == 2
+   lucas(n - 1) + lucas(n - 2)
 end
 
 def summed(n)
